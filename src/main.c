578345ce340c54d7e1a6622da60aff90002a49a2
lesacar <balkangamescroatia@gmail.com>
2024-07-15 13:14:12 +0200

    .

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@ int main() {
     SetTraceLogLevel(LOG_WARNING);
     InitWindow(1000, 540, "x86_64 raylib cars main window");
     InitAudioDevice();
+	int geartops[] = {999,140,195,240,285,330};
     SetTargetFPS(60);
     SetExitKey(KEY_NULL);
     bool window_close = false;
@@ -39,18 +40,19 @@ int main() {
 			speed = speed + GetRandomValue(-10, 10)/40.0f;
 
 		}
-		if (IsKeyDown(KEY_W)) {
+		if (IsKeyDown(KEY_W) && speed < geartops[gear]) {
 			speed += (1.0f/gear)/2.0f;
-		} else if (speed > 0) {
+		}
+		if (!IsKeyDown(KEY_W) && speed > 0) {
 			//speed -= (1.0f/gear)/4.0f;
-			speed -= speed/600.0f;
+			speed -= (speed/300.0f)/24.0f;
 		}
 		if (IsKeyDown(KEY_S)) {
 			if (speed - (gear/1.75f) > 0.0f) {
 				speed -= 0.7f;
 			 } 
 		}
-		if (IsKeyPressed(KEY_E)) {
+		if (IsKeyPressed(KEY_E) && gear < 6) {
 			gear++;
 			speed-= 10.0f;
 		} else if (IsKeyPressed(KEY_Q)) {
@@ -60,8 +62,11 @@ int main() {
 			}
 		}
 
-		pitch = idle_pitch + speed/gear * 0.013;
-
+		if (gear == 1) {
+			pitch = idle_pitch + speed/gear * 0.006;
+		  } else if (gear == 2) {
+			pitch = idle_pitch + speed/gear * 0.006; // TODO
+		  }
 		SetSoundPitch(snd_acc, pitch);
 
 		if (IsKeyDown(KEY_LEFT_CONTROL)) {
